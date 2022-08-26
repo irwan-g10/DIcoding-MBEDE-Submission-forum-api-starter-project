cf0bce1839ae28e49fd95396396c540357e38fac
irwan-g10 <irwangumilar111@gmail.com>
2022-08-26 21:09:55 +0700

    cek1

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -4,7 +4,6 @@ describe("CommentRepository interface", () => {
   it("should throw error when invoke abstract behavior", async () => {
     // Arrange
     const commmentRepository = new CommmentRepository();
-
     // Action and Assert
     await expect(commmentRepository.addComment({})).rejects.toThrowError(
       "COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED"
